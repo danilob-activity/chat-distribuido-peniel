ce7d4d25b03df440c5dc6424f0daa0432c4f1dfe
OzeiasSilva <ozeiassilva2015@gmail.com>
2019-10-05 16:48:45 -0300

    Criação do servidor

--- servidor.java
+++ servidor.java
@@ -22,4 +22,98 @@ public class Servidor extends Thread {
     private InputStreamReader inr;
     private BufferedReader bfr;
 
-}
+    /**
+     * Método construtor
+     * 
+     * @param com do tipo Socket
+     */
+
+    public Servidor(Socket con) {
+        this.con = con;
+        try {
+            in = con.getInputStream();
+            inr = new InputStreamReader(in);
+            bfr = new BufferedReader(inr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * toda vez que um cliente novo chega ao servidor, esse método é acionado e
+     * alocado numa Thread e também fica verificando se existe alguma mensagem nova.
+     * 
+     * Método run
+     */
+
+    public void run() {
+        try {
+
+            String msg;
+            OutputStream ou = this.con.getOutputStream();
+            Writer ouw = new OutputStreamWriter(ou);
+            BufferedWriter bfw = new BufferedWriter(ouw);
+            clientes.add(bfw);
+            nome = msg = bfr.readLine();
+
+            while (!"Sair".equalsIgnoreCase(msg) && msg != null) {
+                msg = bfr.readLine();
+                sendToAll(bfw, msg);
+                System.out.println(msg);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Quando um cliente envia uma mensagem, o servidor recebe e manda esta para
+     * todos os outros clientes conectados.
+     * 
+     * Método usado para enviar mensagem para todos os clients
+     */
+
+    public void sendToAll(BufferedWriter bwSaida, String msg) throws IOException {
+
+        BufferedWriter bwS;
+
+        for (BufferedWriter bw : clientes) {
+            bwS = (BufferedWriter) bw;
+            if (!(bwSaida == bwS)) {
+                bw.write(nome + " -> " + msg + "\r\n");
+                bw.flush();
+            }
+        }
+    }
+
+    /**
+     * Método main
+     */
+
+    public static void main(String[] args) {
+
+        try {
+            // Cria os objetos necessários para instânciar o servidor
+            JLabel lblMessage = new JLabel("Porta do Servidor");
+            JTextField txtPorta = new JTextField("12345");
+            Object[] texts = { lblMessage, txtPorta };
+            JOptionPane.showMessageDialog(null, texts);
+            server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
+            clientes = new ArrayList<BufferedWriter>();
+            JOptionPane.showMessageDialog(null, "Servidor ativo na porta: " + txtPorta.getText());
+
+            while (true) {
+                System.out.println("Aguardando conexão...");
+                Socket con = server.accept();
+                System.out.println("Cliente conectado...");
+                Thread t = new Servidor(con);
+                t.start();
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }// Fim do método main
+} // Fim da classe
